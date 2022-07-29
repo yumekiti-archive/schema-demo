ef015c3af323f7491ab74cab80fe273f0a0bf9f0
yumekiti <yumekiti120440@gmail.com>
2022-07-29 14:34:03 +0900

    `add` schemas

--- api/routers/task.py
+++ api/routers/task.py
@@ -1,11 +1,14 @@
+from typing import List
+
 from fastapi import APIRouter
+
+import schemas.task as task_schema
 
 router = APIRouter()
 
-
-@router.get("/tasks")
+@router.get("/tasks", response_model=List[task_schema.Task])
 async def list_tasks():
-    pass
+    return [task_schema.Task(id=1, title="1つ目のTODOタスク")]
 
 
 @router.post("/tasks")
